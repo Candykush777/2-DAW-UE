cef852c3bdc3cbd5750b5e55910a48b7c18a77f6
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-10-22 18:57:46 +0200

    3

--- Desarrollo Web en Entorno Servidor/Clase_21_10_24/ejemplo_testigo1.php
+++ Desarrollo Web en Entorno Servidor/Clase_21_10_24/ejemplo_testigo1.php
@@ -6,6 +6,34 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php 
+
+$hayDos = false; // variable de valores : true o false
+
+for ($i=0; $i <5 ; $i++) { 
+    
+   $dado = rand(1,6);
+   print "<p>tirada de dado : $dado</p>\n";
+
+if($dado == 2){
+
+    $hayDos = true;
+}
+
+
+} // fin del bucle 
+
+if($hayDos == true){
+
+    print "<p>Ha salido algún dos</p>\n";
+} else{
+
+    print "<p>No ha salido ningún dos</p>";
+
+}
+
+?>
     
 </body>
 </html>
