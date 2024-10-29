beac63b643f6f825907d6c5ff3b1652309183301
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-10-29 18:12:56 +0100

    9

--- Desarrollo Web en Entorno Cliente/Tema_2/Funciones/js/index.js
+++
@@ -1,4 +0,0 @@
-let nombre = "Edu";
-let edad = 43;
-
-console.log(nombre);
---
+++ Desarrollo Web en Entorno Cliente/Tema_2/Funciones/utils/js/index.js
@@ -0,0 +1,44 @@
+//const Swal = require('sweetalert2');
+//import Swal from 'sweetalert2';
+
+let nombre = "Edu";
+//let edad = 43;
+
+console.log(nombre);
+console.log(`Hola ${nombre} es el primer programa en JS`);
+
+//interacción con el usuario
+
+//alert("Bienvenido  a la página");
+//alert(`Bienvenido ${nombre} a la página`); saca info sin al posibilidad de hacer nada
+
+// let edad = prompt("Por favor introduce tu edad"); //retorna un tipo de dato , el que hayas metido, string, number etc
+
+/* let edad = parseInt(prompt("Por favor introduce tu edad"));//string otra manera es donde parseInt - Number 
+if(isNaN(edad)){
+
+    alert("Dato incorrecto")
+}else{
+    alert(`Tu edad es de ${edad}` );
+} */
+
+/* let contiuar = confirm("Estas seguro que quieres contiuar?"); //boolean
+
+if (contiuar) {
+  let edad = Number(prompt("Por favor introduce tu edad")); //string otra manera es donde parseInt - Number
+  if (isNaN(edad)) {
+    alert("Dato incorrecto");
+  } else {
+    alert(`Tu edad es de ${edad}`);
+  }
+} else {
+  alert("No quiere continuar");
+} */
+
+//alert(`Tu edad es de ${edad}`);
+
+Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success"
+  });
