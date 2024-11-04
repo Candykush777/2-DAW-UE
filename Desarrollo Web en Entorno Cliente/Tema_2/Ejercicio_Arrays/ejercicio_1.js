3683eacd6fc4633d9b869dce1305ec592155479c
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-11-04 20:23:34 +0100

    23

--- Desarrollo Web en Entorno Cliente/Tema_2/Ejercicio_Arrays/ejercicio_1.js
+++ Desarrollo Web en Entorno Cliente/Tema_2/Ejercicio_Arrays/ejercicio_1.js
@@ -31,6 +31,71 @@ while(indice<array.length){
 }
 
 
+// con for
 
+for (let i = 0; i < array.length ; i++) {
+
+
+    
+    console.log(array[i]);
+    
+    
+}
+console.log("------------------------");
+
+
+for(const arrays of array){
+
+   
+
+    console.log(arrays);
+    
+}
+
+console.log("------------------------");
+
+// con forEach
+
+array.forEach((element,i) => {
+    console.log(element);
+    
+    
+});
+
+console.log("--------------------------");
+
+//sumarle 1 a todos lso elementos 
+
+const resultado =[];
+for (let i = 0; i < array.length ; i++) {
+
+
+
+resultado.push(array[i]+1);
+    
+    
+    
+    
+}console.log(resultado);
+
+console.log("--------------------");
+
+//calcular la media de todos los elementos del array 
+
+let suma=0; // me estaba dando NaN por noigualar a 0, xq toma valor como undefined
+let media;
+
+
+for (let i = 0; i < array.length ; i++) {
+
+
+    suma += array[i];
+     
+   
+    
+}
+
+media= suma/array.length;
+console.log(media);
 
 
