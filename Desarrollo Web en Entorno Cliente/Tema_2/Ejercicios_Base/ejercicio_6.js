8ab7ee66c7077533e59a18488735a86bf737c1f0
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-11-04 14:02:37 +0100

    20

--- Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Base/ejercicio_6.js
+++ Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Base/ejercicio_6.js
@@ -12,18 +12,44 @@ ejecutará la siguiente funcionalidad:
 - X frases */
 
 let insertFrase;
+let nLEtras;
+let nPalabras;
 
 while (true) {
   insertFrase = prompt("Introduzca una frase : ");
 
-  let nLEtras = insertFrase.length;
+  const sinEspacios=insertFrase.replace(/[.,?¿¡!]/g, "").replace(/\s/g, "");//expresions regulares mas precisas
+
+   nLEtras = sinEspacios.length;
 
   if (nLEtras >= 10) {
     break;;
   }
   else{
-    alert("LA frase tiene menos de 10 letras. Vuelve a Introducir");
+    alert("La frase tiene menos de 10 letras. Vuelve a Introducir");
   }
+
+ // alert("");
 }
-console.log(`La frase introducida es : ${insertFrase}`);
+
+const palabras=insertFrase.trim().split(/\s+/); //expresiones regulares hacen mas preciso y limpio el código
+nPalabras = palabras.length;
+
+const nFrases=insertFrase.split(/[.?!]/).filter(frase =>frase.trim().length > 0).length;
+
+alert(`La frase introducida es : ${insertFrase}, 
+       El número de letras sin contar espacios es : ${nLEtras}, 
+       El número de palabras de la frase es : ${nPalabras}, 
+       El número de frases es : ${nFrases}`);
+
+
+/* console.log(`La frase introducida es : ${insertFrase}`);
+console.log(`El número de letras sin contar espacios es : ${nLEtras}`);
+console.log(`El número de palabras de la frase es : ${nPalabras}`);
+console.log(`El número de frases es : ${nFrases}`); */
+
+
+
+
+
 
