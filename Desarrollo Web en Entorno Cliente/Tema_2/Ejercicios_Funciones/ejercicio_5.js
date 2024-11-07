c09d5aceedc24739123838c017a915df919ac087
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-11-07 01:55:21 +0100

    29

--- Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Funciones/ejercicio_5.js
+++ Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Funciones/ejercicio_5.js
@@ -24,3 +24,83 @@ hipoteca.js:25 Pago correspondiente al mes 6 con una cantidad de 475000
 hipoteca.js:25 Pago correspondiente al mes 7 con una cantidad de 470833
 hipoteca.js:25 Pago correspondiente al mes 8 con una cantidad de 466667
 ``` */
+
+console.log("----------HOMEBUYER APP----------");
+
+let prestamo;
+let precio;
+let nAnios;
+let interes;
+
+while (true) {
+  let InsertPrecio = prompt("Introduzca el importe de la compra : ");
+  let InsertPrestamo = prompt("Introduzca credito necesario : ");
+  let InsertnAnios = prompt("Introduzca el nº de años amortizar : ");
+  let InsertInteres = prompt("Introduzca el % de interés a aplicar : ");
+
+  precio = parseFloat(InsertPrecio);
+  prestamo = parseFloat(InsertPrestamo);
+  nAnios = parseInt(InsertnAnios, 10);
+  interes = parseFloat(InsertInteres);
+
+  if (
+    !isNaN(precio) &&
+    !isNaN(prestamo) &&
+    !isNaN(nAnios) &&
+    !isNaN(interes) &&
+    prestamo > 0 &&
+    precio > 0 &&
+    nAnios > 0 &&
+    interes > 0
+  ) {
+    break;
+  } else {
+    alert("Error, has introducido datos incorrectos");
+  }
+}
+
+
+
+
+// Función para calcular el pago mensual
+function calcularPagoMensual() {
+  const tasaInteresMensual = interes / (100 / 12);  // Tasa de interés mensual
+  const totalMeses = nAnios * 12;  // Número total de meses
+
+  // Fórmula de pago mensual
+  const pagoMensual = (prestamo * tasaInteresMensual) / (1 - Math.pow(1 + tasaInteresMensual, -totalMeses));
+
+  return pagoMensual;
+}
+const cuotaMensual = calcularPagoMensual();
+const totalConIntereses = cuotaMensual * nAnios * 12 ;
+const aportacion=precio-prestamo;
+let totalReal=totalConIntereses+aportacion;
+
+console.log(
+    `Vas a pagar una cantidad de ${totalReal.toFixed(0)} ya con intereses en ${nAnios * 12} mensualidades con un importe mensual de ${cuotaMensual.toFixed(0)}`
+  );
+  
+  // Tabla de amortización
+  let saldoRestante = totalConIntereses ;
+  
+  for (let mes = 1; mes <= nAnios * 12; mes++) {
+    saldoRestante -= pagoMensual;
+    console.log(
+      `Pago correspondiente al mes ${mes} con una cantidad de ${saldoRestante.toFixed(0)}`
+    );
+  }
+
+
+
+
+
+/* - Valor de la casa 450000€
+- Dinero pedido al banco: 200000€
+- Años a pagar: 15
+- Intereses aplicados: 1,5%
+La tabla de amortización será la siguiente
+```javascript
+hipoteca.js:17 Vas a pagad una cantidad de 500000 ya con intereses en 120
+mensualidades con un importe mensual de 4167
+hipoteca.js:25 Pago correspondiente al mes 1 con una cantidad de 495833 */
