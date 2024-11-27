0763dff6de20f06a0198aa47378c674947519df4
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-11-27 20:51:26 +0100

    46

---
+++ Desarrollo Web en Entorno Cliente/Tema_2/Clase_25_Nov/js/index.js
@@ -0,0 +1,11 @@
+let inputNombre =document.querySelector("#nombreInput");
+let inputMail =document.querySelector("#emailInput");
+let inputPass =document.querySelector("#passInput");
+let inputGenero =document.querySelector("#generoInput");
+let checkGuardar =document.querySelector("#checkInput");
+let btnGuardar =document.querySelector("#btnGuardar");
+
+btnGuardar.addEventListener('click',(e)=> {
+
+    
+})
--- Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Objetos/equipo.js
+++ Desarrollo Web en Entorno Cliente/Tema_2/Ejercicios_Objetos/equipo.js
@@ -4,16 +4,25 @@ class Jugador {
     this.posicion = posicion;
     this.valor = valor;
   }
-
 }
 class Equipo {
-  constructor(nombre,  presupuesto) {
+  constructor(nombre, presupuesto) {
     this.nombre = nombre;
     this.plantilla = [];
     this.presupuesto = presupuesto;
   }
 
   ficharJugador(jugador) {
+    if (this.presupuesto < jugador.valor) {
+      console.log(
+        `El equipo ${this.nombre} no tiene presupuesto para fichar a este jugador`
+      );
+    } else {
+      this.presupuesto -= jugador.valor;
+      console.log(
+        `El Equipo ${this.nombre}  ha fichado a ${jugador.nombre}, con la posicion ${jugador.posicion}, Este es el presupuesto restante : ${this.presupuesto} al equipo ¡¡`
+      );
+    }
     this.plantilla.push(jugador); //Se agrega al jugador
   }
 
@@ -26,11 +35,14 @@ class Equipo {
       );
     });
   }
+
+  presupuestoEquipos() {
+    //Me gustaria poner aqui un resumen del presupuesto que queda en lso equipos despues de los fichajes
+
+    console.log(
+      `Presupuesto restante del equipo ${this.nombre}: ${this.presupuesto} €`
+    );
+  }
 }
 
-
-
 export { Jugador, Equipo };
-
-
-
