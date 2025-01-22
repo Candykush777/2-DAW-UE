3ae66bdd78e3af1a86c6df3c7b0cc2c04e5094d7
Candykush <eduardoafan.eae@gmail.com>
2025-01-22 14:24:11 +0100

    8 finalizado

--- Desarrollo Web en Entorno Cliente/Bases_Notion_Tema4/Ejercicio8_Asincronia/Utils/js/index.js
+++ Desarrollo Web en Entorno Cliente/Bases_Notion_Tema4/Ejercicio8_Asincronia/Utils/js/index.js
@@ -1,12 +1,20 @@
 let usuarios = [];
 
-let carrito =[];
+let carrito = [];
 
-let precioTotal=0;
+let precioTotal = 0;
 
 let divContainer = document.querySelector("#productos");
 
-let btnAgregarCarrito = document.querySelector("#botonCarrito");
+let eliminarCarritoBtn = document.querySelector("#eliminarCarrito");
+
+let filtrado = document.querySelector("#filtrar");
+
+filtrado.addEventListener("change", aplicarFiltro);
+
+let filtradoCategoria = document.querySelector("#filtrarCategoria");
+
+filtradoCategoria.addEventListener("change", aplicarFiltro);
 
 function cargarProductos() {
   fetch("https://fakestoreapi.com/products")
@@ -20,8 +28,8 @@ function cargarProductos() {
         icon: "success",
       });
 
-      usuarios.forEach((usuario) => {
-        pintarCard(usuario);
+      usuarios.forEach((producto) => {
+        pintarCard(producto);
       });
     })
     .catch((error) => {
@@ -34,66 +42,219 @@ function cargarProductos() {
     });
 }
 
-function pintarCard(usuario) {
+function pintarCard(producto) {
   let columna = document.createElement("div");
-columna.classList.add("col-4", "mb-4"); 
+  columna.classList.add("col-4", "mb-4");
 
-let card = document.createElement("div");
-card.classList.add("card", "shadow");  
+  let card = document.createElement("div");
+  card.classList.add("card", "shadow");
 
-let cardBody = document.createElement("div");
-cardBody.classList.add("card-body");
+  let cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
-let title = document.createElement("h5");
-title.classList.add("card-title");  
-title.innerHTML = `<b>Nombre producto:</b> ${usuario.title}`;
+  let title = document.createElement("h5");
+  title.classList.add("card-title");
+  title.innerHTML = `<b>Nombre producto:</b> ${producto.title}`;
 
-let price = document.createElement("p");
-price.classList.add("card-text", "text-primary");  
-price.innerHTML = `<b>Precio: </b> ${usuario.price}`;
+  let price = document.createElement("p");
+  price.classList.add("card-text", "text-primary");
+  price.innerHTML = `<b>Precio: </b> ${producto.price}`;
 
-let descripcion = document.createElement("p");
-descripcion.classList.add("card-text", "text-muted");
-descripcion.innerHTML = `<b>Descripción: </b> ${usuario.description}`;
+  let descripcion = document.createElement("p");
+  descripcion.classList.add("card-text", "text-muted");
+  descripcion.innerHTML = `<b>Descripción: </b> ${producto.description}`;
 
-let btnAgregarCarrito = document.createElement("button");
-btnAgregarCarrito.classList.add("btn", "btn-success", "btn-agregar", "mt-2"); 
-btnAgregarCarrito.textContent = "Agregar al Carrito";
+  let categoria = document.createElement("h6");
+  categoria.classList.add("card-text");
+  categoria.innerHTML = `<b>Categoria: </b> ${producto.category}`;
+
+  let btnAgregarCarrito = document.createElement("button");
+  btnAgregarCarrito.classList.add("btn", "btn-success", "btn-agregar", "mt-2");
+  btnAgregarCarrito.textContent = "Agregar al Carrito";
 
   cardBody.appendChild(title);
   cardBody.appendChild(price);
   cardBody.appendChild(descripcion);
+  cardBody.appendChild(categoria);
   cardBody.appendChild(btnAgregarCarrito);
   card.appendChild(cardBody);
   columna.appendChild(card);
 
   divContainer.appendChild(columna);
 
-  btnAgregarCarrito.addEventListener("click",(e)=>{
-
-
-
-
-
-  } )
+  btnAgregarCarrito.addEventListener("click", (e) => {
+    agregarAlCarrito(producto);
+  });
 }
 
-function agregarAlCarrito(usuario) {
+function agregarAlCarrito(producto) {
+  Swal.fire({
+    title: "¡Producto agregado!",
+    text: `${producto.title} ha sido añadido al carrito.`,
+    icon: "success",
+    timer: 2000,
+  });
 
-  carrito.push(usuario);
+  setTimeout(() => {
+    carrito.push(producto);
 
-precioTotal += usuario.price;
+    precioTotal += producto.price;
 
-document.getElementById("precioTotal").textContent=`$${precioTotal.toFixed(2)}`
+    document.getElementById("precioTotal").textContent = `${precioTotal.toFixed(
+      2
+    )}€`;
 
-//mostrar elproducto en el carrito 
-  
+    //tenemos que mostrar el producto en el carrito
+
+    mostrarProductoCarrito(producto);
+  }, 2000);
 }
 
+//mostrar elproducto en el carrito
 
+function mostrarProductoCarrito(producto) {
+  let listaCarrito = document.querySelector("#listaCarrito");
+
+  let productoEnCarrito = document.createElement("li");
+
+  productoEnCarrito.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+
+  productoEnCarrito.innerHTML = `${producto.title} Precio: ${producto.price}€`;
+
+  let eliminarBtn = document.createElement("button");
+  eliminarBtn.classList.add("btn", "btn-danger", "btn-sm");
+  eliminarBtn.textContent = "Eliminar";
+
+  //elinarmos el producto del carrito
+
+  eliminarBtn.addEventListener("click", (e) => {
+    carrito = carrito.filter((item) => item !== producto);
+
+    precioTotal -= producto.price;
+
+    //ahora actualizamos su precio
+
+    document.getElementById(
+      "precioTotal"
+    ).textContent = `Total: ${precioTotal.toFixed(2)}€`;
+
+    productoEnCarrito.remove();
+  });
+
+  productoEnCarrito.appendChild(eliminarBtn);
+  listaCarrito.appendChild(productoEnCarrito);
+}
+
+//ahora queremos eliminar todo el carrito y dejarlo vacio
+
+eliminarCarritoBtn.addEventListener("click", (e) => {
+  carrito = [];
+  precioTotal = 0;
+  let precioTotalCarrito = document.getElementById("precioTotal");
+  let listaCarritoProducto = document.getElementById("listaCarrito");
+
+  //actualizamos el DOM
+  precioTotalCarrito.textContent = `Total: ${precioTotal.toFixed(2)}€`;
+
+  listaCarritoProducto.innerHTML = "";
+});
+
+function aplicarFiltro() {
+  let filtrar = document.querySelector("#filtrar").value;
+  let filtrarCategoria = document.querySelector("#filtrarCategoria").value;
+
+  // Inicializamos productosFiltrados con todos los productos
+  let productosFiltrados = [...usuarios];
+
+  // Obtener precio máximo y mínimo de los productos
+  let precioMaximo = Math.max(...usuarios.map((producto) => producto.price));
+  let precioMinimo = Math.min(...usuarios.map((producto) => producto.price));
+
+  // Filtrar por precio
+  if (filtrar) {
+    switch (filtrar) {
+      case "1":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.price === precioMinimo
+        );
+        break;
+
+      case "2":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.price === precioMaximo
+        );
+        break;
+
+      case "3":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.price <= 100
+        );
+        break;
+
+      case "4":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.price > 100
+        );
+        break;
+
+      default:
+        // Si no hay filtro de precio, asignamos todos los productos
+        productosFiltrados = productosFiltrados;
+        break;
+    }
+  }
+
+  // Filtrar por categoría
+  if (filtrarCategoria) {
+    switch (filtrarCategoria) {
+      case "5":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.category === "men's clothing"
+        );
+        break;
+
+      case "6":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.category === "women's clothing"
+        );
+        break;
+
+      case "7":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.category === "electronics"
+        );
+        break;
+
+      case "8":
+        productosFiltrados = productosFiltrados.filter(
+          (producto) => producto.category === "jewelery"
+        );
+        break;
+
+      default:
+        // Si no hay filtro de categoría, asignamos todos los productos
+        productosFiltrados = productosFiltrados;
+        break;
+    }
+  }
+
+  // Mostrar los productos filtrados
+  mostrarProductos(productosFiltrados);
+}
+
+// Función para mostrar los productos filtrados
+function mostrarProductos(productos) {
+  divContainer.innerHTML = "";
+
+  productos.forEach((producto) => {
+    pintarCard(producto);
+  });
+}
 
 //esto deberia ir despues de todo fuera de todo ¡¡
 cargarProductos();
-
-
-
